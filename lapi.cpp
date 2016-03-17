6034fe696f1aba2efa67644654068f189f792ea6
Maxime Killinger <maxime.killinger@epitech.eu>
2016-03-17 11:49:31 +0100

    More function //dosen't work

--- lapi.cpp
+++ lapi.cpp
@@ -5,7 +5,7 @@
 ** Login   <killin_m@epitech.net>
 **
 ** Started on  Tue Mar  8 13:50:18 2016 Maxime Killinger
-** Last update Wed Mar 16 17:25:49 2016 Maxime Killinger
+** Last update Thu Mar 17 11:44:32 2016 Maxime Killinger
 */
 
 #include <opencv2/core/core.hpp>
@@ -14,10 +14,24 @@
 #include <iostream>
 #include <math.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <opencv2/imgproc/imgproc_c.h>
+
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define sign(x) ((x) > 0 ? 1 : -1)
+
+#define STEP_MIN 5
+#define STEP_MAX 100
 
 using namespace cv;
 using namespace std;
 
+IplImage *image;
+CvPoint objectPos = cvPoint(-1, -1);
+int h = 0, s = 0, v = 0, tolerance = 10;
 int		thresh = 50;
 int		N = 11;
 const char	*wndname = "LAPI";
@@ -31,23 +45,23 @@ static double	angle(Point pt1, Point pt2, Point pt0)
   return (dx1*dx2 + dy1*dy2)/sqrt((dx1*dx1 + dy1*dy1)*(dx2*dx2 + dy2*dy2) + 1e-10);
 }
 
-static void	drawSquares(Mat& image, const vector<vector<Point> >& squares)
+static void	drawSquares(Mat& frame, const vector<vector<Point> >& squares)
 {
   for (size_t i = 0; i < squares.size(); i++)
     {
       const Point* p = &squares[i][0];
       int n = (int)squares[i].size();
-      polylines(image, &p, &n, 1, true, Scalar(0,102,51), 3, CV_AA);
+      polylines(frame, &p, &n, 1, true, Scalar(0,102,51), 3, CV_AA);
     }
-  imshow(wndname, image);
+  imshow(wndname, frame);
 }
 
-static void	findSquares( const Mat& image, vector<vector<Point> >& squares )
+static void	findSquares( const Mat& frame, vector<vector<Point> >& squares )
 {
   squares.clear();
-  Mat pyr, timg, gray0(image.size(), CV_8U), gray;
-  pyrDown(image, pyr, Size(image.cols/2, image.rows/2));
-  pyrUp(pyr, timg, image.size());
+  Mat pyr, timg, gray0(frame.size(), CV_8U), gray;
+  pyrDown(frame, pyr, Size(frame.cols/2, frame.rows/2));
+  pyrUp(pyr, timg, frame.size());
   vector<vector<Point> > contours;
   for (int c = 0; c < 3; c++)
     {
@@ -85,6 +99,80 @@ static void	findSquares( const Mat& image, vector<vector<Point> >& squares )
   }
 }
 
+CvPoint binarisation(IplImage* frame, int *nbPixels) {
+    int x, y;
+    CvScalar pixel;
+    IplImage *hsv, *mask;
+    IplConvKernel *kernel;
+    int sommeX = 0, sommeY = 0;
+    *nbPixels = 0;
+    mask = cvCreateImage(cvGetSize(frame), frame->depth, 1);
+    hsv = cvCloneImage(frame);
+    cvCvtColor(frame, hsv, CV_BGR2HSV);
+    cvInRangeS(hsv, cvScalar(h - tolerance -1, s - tolerance, 0), cvScalar(h + tolerance -1, s + tolerance, 255), mask);
+    kernel = cvCreateStructuringElementEx(5, 5, 2, 2, CV_SHAPE_ELLIPSE);
+    cvDilate(mask, mask, kernel, 1);
+    cvErode(mask, mask, kernel, 1);
+    for(x = 0; x < mask->width; x++) {
+        for(y = 0; y < mask->height; y++) {
+            if(((uchar *)(mask->imageData + y*mask->widthStep))[x] == 255) {
+                sommeX += x;
+                sommeY += y;
+                (*nbPixels)++;
+            }
+        }
+    }
+    cvShowImage("Mask", mask);
+    cvReleaseStructuringElement(&kernel);
+    cvReleaseImage(&mask);
+    cvReleaseImage(&hsv);
+    if(*nbPixels > 0)
+        return cvPoint((int)(sommeX / (*nbPixels)), (int)(sommeY / (*nbPixels)));
+    else
+        return cvPoint(-1, -1);
+}
+
+void addObjectToVideo(IplImage* frame, CvPoint objectNextPos, int nbPixels) {
+    int objectNextStepX, objectNextStepY;
+    if (nbPixels > 10) {
+        if (objectPos.x == -1 || objectPos.y == -1) {
+            objectPos.x = objectNextPos.x;
+            objectPos.y = objectNextPos.y;
+        }
+        if (abs(objectPos.x - objectNextPos.x) > STEP_MIN) {
+            objectNextStepX = max(STEP_MIN, min(STEP_MAX, abs(objectPos.x - objectNextPos.x) / 2));
+            objectPos.x += (-1) * sign(objectPos.x - objectNextPos.x) * objectNextStepX;
+        }
+        if (abs(objectPos.y - objectNextPos.y) > STEP_MIN) {
+            objectNextStepY = max(STEP_MIN, min(STEP_MAX, abs(objectPos.y - objectNextPos.y) / 2));
+            objectPos.y += (-1) * sign(objectPos.y - objectNextPos.y) * objectNextStepY;
+        }
+    } else {
+        objectPos.x = -1;
+        objectPos.y = -1;
+    }
+    if (nbPixels > 10)
+        cvDrawCircle(frame, objectPos, 15, CV_RGB(255, 0, 0), -1);
+    cvShowImage("Color Tracking", frame);
+}
+
+void getObjectColor(int event, int x, int y, int flags, void *param = NULL) {
+    CvScalar pixel;
+    IplImage *hsv;
+
+    if(event == CV_EVENT_LBUTTONUP) {
+        hsv = cvCloneImage(image);
+        cvCvtColor(image, hsv, CV_BGR2HSV);
+        pixel = cvGet2D(hsv, y, x);
+        h = (int)pixel.val[0];
+        s = (int)pixel.val[1];
+        v = (int)pixel.val[2];
+        cvReleaseImage(&hsv);
+
+    }
+
+}
+
 int		main(int ac, char **av)
 {
   double	dWidth;
@@ -93,6 +181,9 @@ int		main(int ac, char **av)
   int		width;
   int		height;
   uchar		*raw;
+  IplImage *hsv;
+  int nbPixels;
+  CvPoint objectNextPos;
 
   VideoCapture	cap(0);
   if (!cap.isOpened())
@@ -110,10 +201,18 @@ int		main(int ac, char **av)
       Mat frame;
       bSuccess = cap.read(frame);
       if (!bSuccess)
-	    {
-	        cout << "Cannot read a frame from video stream" << endl;
-	        break;
-	    }
+        {
+	  cout << "Cannot read a frame from video stream" << endl;
+	  break;
+        }
+//      objectNextPos = binarisation(frame, &nbPixels);
+//      addObjectToVideo(frame, objectNextPos, nbPixels);
+      cvNamedWindow("Color Tracking", CV_WINDOW_AUTOSIZE);
+      cvNamedWindow("Mask", CV_WINDOW_AUTOSIZE);
+      cvMoveWindow("Color Tracking", 0, 100);
+      cvMoveWindow("Mask", 650, 100);
+      cvMoveWindow("LAPI", 325, 580);
+//      cvSetMouseCallback("Color Tracking", getObjectColor);
       Mat grey;
       cvtColor(frame, grey, CV_BGR2GRAY);
       width = frame.cols;
@@ -123,16 +222,16 @@ int		main(int ac, char **av)
       findSquares(frame, squares);
       drawSquares(frame, squares);
       if (waitKey(30) == 27)
-	    {
-	        cvDestroyAllWindows();
-	        cout << "Escape key is pressed by user" << endl;
-	        break;
-	    }
+	{
+	  cvDestroyAllWindows();
+	  cout << "Escape key is pressed by user" << endl;
+	  break;
+	}
       if (getWindowProperty("LAPI", 1) == -1)
-	    {
-            cvDestroyAllWindows();
-	        break;
-	    }
+	{
+	  cvDestroyAllWindows();
+	  break;
+	}
     }
   return (0);
 }
